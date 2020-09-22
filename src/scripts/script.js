3e32b1a89175c21dfed2c2c74d2fa164e796a0e7
Alexander Vasiliev <selado@ya.ru>
2020-09-22 03:54:57 +0300

    add BEM structure

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,23 +1,14 @@
 import '../pages/index.css';
-import DarkTheme from '../js/components/DarkTheme.js';
-import {
-  checkbox,
-  checkboxInput,
-} from '../js/constants/constants.js';
+// import DarkTheme from '../js/components/DarkTheme.js';
+// import {
+//   checkbox,
+//   checkboxInput,
+// } from '../js/constants/constants.js';
 
 'use strict';
 
 (function() {
 
-  const darkTheme = new DarkTheme();
 
-  if (localStorage.getItem('darkTheme') === 'On') {
-    darkTheme.turnOn();
-    checkboxInput.checked = true;
-  }
-
-  checkbox.addEventListener( 'change', function() {
-    darkTheme.toggleStyles();
-  });
 
 })();
